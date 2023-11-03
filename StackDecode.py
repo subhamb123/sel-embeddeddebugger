910e8a3c254336283bc4af7499de3a520c956e64
hpotter94 <howard.potter@wsu.edu>
2023-11-03 12:11:45 -0700

    Updating to pasrse text file
    Modified code to take stack.txt as outputted for example.

--- StackDecode.py
+++ StackDecode.py
@@ -5,9 +5,7 @@ import struct
 import subprocess
 
 def extract_program_counters(stack_data):
-    program_counters = []
-    stack_format = 'Q' * (len(stack_data) // struct.calcsize('Q'))
-    program_counters = struct.unpack(stack_format, stack_data)
+    program_counters = [address for address, _ in stack_data]
     return program_counters
 
 def extract_function_names(elf_file):
@@ -27,18 +25,33 @@ def map_program_counters_to_functions(program_counters, function_names):
                 pc_to_function[pc] = func_name
     return pc_to_function
 
+def read_stack_data_from_file(file_path):
+    stack_data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.startswith("Address:") and "Value:" in line:
+                address_value = line.strip().replace('Address:', '').replace('Value:', '').split(',')
+                address = int(address_value[0].strip(), 16)
+                value = int(address_value[1].strip(), 16)
+                stack_data.append((address, value))
+    return stack_data
+
+
 def main():
     # Stand in for stack data - needs to be replaced with actual stack data.
-    stack_data = b'\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00'
+    stack_data = read_stack_data_from_file('stack.txt')
     
     # Extract program counters from stack data
     program_counters = extract_program_counters(stack_data)
     print("Program Counters:", program_counters)
     
     # Extract function names from .elf file
-    elf_file = "C:\\projects\\sel-debugger\\SEL-Debugger\\Debug\\SEL-Debugger.elf"
+    elf_file = "C:\\projects\\sel-debugger\\SELDebugger\\Debug\\SELDebugger.elf"
     functions = extract_function_names(elf_file)
     print("Function Names:", functions)
+
+    objdump_output = subprocess.check_output(["C:\\Xilinx\\Vitis\\2023.1\\gnu\\aarch64\\nt\\aarch64-none\\bin\\aarch64-none-elf-objdump.exe", "-t", elf_file])
+    print(objdump_output.decode('utf-8'))
     
     # Map program counters to function names
     pc_to_function_mapping = map_program_counters_to_functions(program_counters, functions)
